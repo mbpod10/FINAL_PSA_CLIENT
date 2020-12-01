c9b3a32bf0db3d2d0d6535242bed6e685c0c6755
M. Brock Podgurski <mbpod10@gmail.com>
2020-11-30 19:12:44 -0500

    made a really nice login

--- src/components/FormComponents/Login.js
+++ src/components/FormComponents/Login.js
@@ -1,12 +1,45 @@
 import { useCookies } from 'react-cookie'
 import React, { useEffect } from "react"
-import { Switch, Route } from "react-router-dom"
+import { Link } from "react-router-dom"
+
+import { faUser } from '@fortawesome/free-solid-svg-icons'
+import { faLock } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import './form-styles.css'
 
 const Login = (props) => {
   return (
-    <img className=""
-      src="https://0201.nccdn.net/1_2/000/000/175/968/Logo1.png"
-      alt="psa" />
+    <>
+      <h1 className="form-title">Login</h1>
+
+      <div className="form">
+        <div className="form-container">
+          <label for="user" className="login-label"><FontAwesomeIcon icon={faUser} /></label>
+          <input id="user" className="" type="text" placeholder="Username" />
+        </div>
+      </div>
+
+      <div className="form">
+        <div className="form-container">
+          <label for="pass" className="login-label"><FontAwesomeIcon icon={faLock} /></label>
+          <input id="pass" className="" type="password" placeholder="Password" />
+        </div>
+      </div>
+
+      <div className="form">
+        <div className="form-container">
+          <button className="form-submit">Login </button>
+        </div>
+      </div>
+
+      <div className="form">
+        <div className="form-container">
+          <h5>Have An Account? <Link to="/register"> Register Here</Link></h5>
+        </div>
+      </div>
+
+    </>
   )
 }
 
